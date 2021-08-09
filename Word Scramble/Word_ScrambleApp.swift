dbee806ad14df0577cf38653066c8a9b98b4bed9
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-08-09 11:42:30 +0100

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by MAC on 9/8/2021.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
